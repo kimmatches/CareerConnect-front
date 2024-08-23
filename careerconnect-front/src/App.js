0ecfd7d48773c6510c871ae5b9fe67b319171fc9
parkchanil <0114chan@gmail.com>
2024-08-23 10:29:03 +0900

    commit

--- careerconnect-front/src/App.js
+++ careerconnect-front/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Chat from './components/Chat';
+import ChatRoom from './components/ChatRoom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );ssss
-}
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/chat/:roomId" element={<ChatRoom />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+  );
+};
 
 export default App;
